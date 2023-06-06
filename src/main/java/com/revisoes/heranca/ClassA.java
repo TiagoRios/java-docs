d2383b7e4942c849a711411bda097ca0d1ce2b66
Tiago Rios <tiago_pc9@hotmail.com>
2023-06-06 14:02:22 -0300

    Exemplos que ao redefinir sobrescreve, esconde ou da erro.

---
+++ src/main/java/com/revisoes/heranca/ClassA.java
@@ -0,0 +1,11 @@
+package com.revisoes.heranca;
+
+public class ClassA {
+    public void methodOne(int i){}
+
+    public void methodTwo(int i){}
+    
+    public static void methodThree(int i){}
+    
+    public static void methodFour(int i){}
+}
