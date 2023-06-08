f72a380f1ee6919f63b01c0241e96fa7122ec50b
Tiago Rios <tiago_pc9@hotmail.com>
2023-06-07 23:16:04 -0300

    Generics: Métodos genèricos, parãmetros tipos limitados, tipos de inferência, herança e subtipos.

---
+++ src/main/java/com/revisoes/genericos/Box.java
@@ -0,0 +1,66 @@
+package com.revisoes.genericos;
+
+import java.io.Serializable;
+import java.util.Calendar;
+
+/**
+ * Classe genérica que demonstra a utilização de parâmetros de limites únicos e
+ * limites multiplos, construtor genérica, métodos genéricos, inferência na
+ * instanciação e na invocação de métodos.
+ * 
+ * <p>{@code NomeClasse<T [ extends outraClasse e/ou interface's']> }
+ */
+public class Box<T extends Number> {
+
+    private T t;
+    private String toStringDoObjeto;
+
+    Box() {
+    }
+
+    // Nessa implementação não importa o tipo, ele será transformado em string.
+    <U> Box(U u) {
+        this.toStringDoObjeto = u.toString();
+    }
+
+    public void set(T t) {
+        this.t = t;
+    }
+
+    public T get() {
+        return t;
+    }
+
+    public String getToStringDoObjeto() {
+        return toStringDoObjeto;
+    }
+
+    // Aplicando parâmetros de tipo limitado. Limites multiplos.
+    public <U extends Number & Serializable> void inspect(U u) {
+        System.out.println("T: " + t.getClass().getName());
+        System.out.println("U: " + u.getClass().getName());
+    }
+
+    public static void main(String[] args) {
+
+        // Inferência na instanciação.
+        Box<Short> shortBox = new Box<>();
+        shortBox.set(Short.valueOf("10")); // java.lang.Short
+
+        // Sem inferência.
+        shortBox.<Double>inspect(99.22); // java.lang.Double
+
+        // Inferência em método genérico.
+        shortBox.inspect(99); // java.lang.Integer
+
+        // error: método está limitado: <U extends Number & Serializable>
+        // integerBox.inspect("some text");
+
+        /* Chamando construtor genèrico */
+        Box<Byte> byteBox = new Box<>(Calendar.getInstance().getTime());
+        System.out.println(byteBox.getToStringDoObjeto()); // Data e hora em String.
+        byteBox.set(Byte.valueOf("-128")); // Faixa entre -128 e 127
+        byteBox.inspect(3D); // Double
+
+    }
+}
---
+++ src/main/java/com/revisoes/genericos/Pair.java
@@ -0,0 +1,31 @@
+package com.revisoes.genericos;
+
+/**
+ * Classe genérica que representa pares de chave e valor.
+ */
+public class Pair<K, V> {
+
+    private K key;
+    private V value;
+
+    public Pair(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public void setKey(K key) {
+        this.key = key;
+    }
+
+    public void setValue(V value) {
+        this.value = value;
+    }
+
+    public K getKey() {
+        return this.key;
+    }
+
+    public V getValue() {
+        return this.value;
+    }
+}
