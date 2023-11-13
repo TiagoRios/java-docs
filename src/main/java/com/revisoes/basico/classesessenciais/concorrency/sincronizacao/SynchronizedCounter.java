cc9b9b8e9c5e386ec9d97e2b35d0a02c0962edf9
Tiago Rios <tiago_pc9@hotmail.com>
2023-11-12 21:21:17 -0300

    Métodos sincronizados.

---
+++ src/main/java/com/revisoes/basico/classesessenciais/concorrency/sincronizacao/SynchronizedCounter.java
@@ -0,0 +1,35 @@
+package com.revisoes.basico.classesessenciais.concorrency.sincronizacao;
+
+/**
+ * <p>
+ * Classe que demonstra como sincronizar métodos.
+ * </p>
+ * <p>
+ * A sincronização de métodos ajuda a evitar interferência de thread e erros de
+ * consistência de memória.
+ * </p>
+ * <p>
+ * Adicione a palavra chave {@code synchronized} na declaração do método para
+ * torná-lo síncrono.
+ * </p>
+ * <p>
+ * <strong>Obs:</strong> Construtores não podem ser sincronizados.
+ * </p>
+ * <p>
+ * </p>
+ */
+public class SynchronizedCounter {
+    private int count = 0;
+
+    public synchronized void increment() {
+        count++;
+    }
+
+    public synchronized void decrement() {
+        count--;
+    }
+
+    public synchronized int value() {
+        return count;
+    }
+}
